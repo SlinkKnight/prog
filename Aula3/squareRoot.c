82dc2652274fb0e3789eb0a5ab305656dfac9de1
“VicenzoStopa” <vicenzostop@outlook.com>
2025-03-21 09:58:57 -0300

    atualizada Aula3

--- Aula3/squareRoot.c
+++ Aula3/squareRoot.c
@@ -13,7 +13,7 @@ int main() {
     if(respostaRaizQuadrada == -1) {
         printf("vai tomar no cu");
     } else {
-        printf("O resultado da raiz de %f: %.0f", valor, respostaRaizQuadrada);
+        printf("O resultado da raiz de %.1f: %.2f", valor, respostaRaizQuadrada);
     }
     return 0;
 }
