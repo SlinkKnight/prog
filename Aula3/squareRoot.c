6f4285cacee6864e482d4c9dc76ad68486c25a18
“VicenzoStopa” <vicenzostop@outlook.com>
2025-03-28 07:40:12 -0300

    finalizada Aula3

--- Aula3/squareRoot.c
+++ Aula3/squareRoot.c
@@ -6,14 +6,9 @@ int main() {
     float valor;
     float respostaRaizQuadrada;
 
-    printf("Informe o valor que se deseja calcular a raiz quadrada >> ");
+    printf("Informe o valor que se deseja calcular >> ");
     scanf("%f", &valor);
     respostaRaizQuadrada = sqrt(valor);
-
-    if(respostaRaizQuadrada == -1) {
-        printf("vai tomar no cu");
-    } else {
-        printf("O resultado da raiz de %.1f: %.2f", valor, respostaRaizQuadrada);
-    }
+    printf("O resultado da raiz de %.1f: %.2f", valor, respostaRaizQuadrada);
     return 0;
 }
