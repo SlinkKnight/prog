a605afee4f98fe0deeb6a589a2e21f5aeab37eea
“VicenzoStopa” <vicenzostop@outlook.com>
2025-04-26 22:00:05 -0300

    aula7

--- Aula7/opCondicional.c
+++ Aula7/opCondicional.c
@@ -3,9 +3,13 @@
 
 int main() {
 
+    char ou;
     int c = 0;
-    int valor = (c == 0)? 1:10;
-    printf("%d\n", valor);
+    int valor = (c == 0)? 1:0;
+
+    scanf("%i", &c);
+    setbuf(stdin, NULL);
+    scanf("%c", &ou);
 
     return 0;
 }
