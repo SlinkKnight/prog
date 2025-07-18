34d8e41232466c279ba4452b254c1da403b9d343
“VicenzoStopa” <vicenzostop@outlook.com>
2025-07-18 09:08:59 -0300

    OS function

--- trimestre2/Aula5/Elevador.c
+++ trimestre2/Aula5/Elevador.c
@@ -4,32 +4,39 @@
 
 int main() {
 
+    
     int andar = 0;
     int andarDesejado;
+    
+    printf("\033[H\033[J");
     printf("Andar atual %i\n", andar);
 
    for ( ; ; )
    {
-    printf("Digite o andar desejado (0-6) >> ");
+    printf("Digite o andar desejado (0-5) >> ");
     scanf("%i", &andarDesejado);
+    printf("\033[H\033[J");
+
         if (andar != andarDesejado) {
             if (andarDesejado < 6 && andarDesejado > -1) {
                 if (andarDesejado < andar) {
-                    printf("Elevador descendo\n");
                     for (andar; andarDesejado <= andar; andar--) {
+                        printf("Elevador descendo\n");
                         printf("%i\n", andar);
                         sleep(1);
+                        printf("\033[H\033[J");
                     }
                 } else {                    
-                    printf("Elevador subindo\n");
                     for (andar; andarDesejado >= andar; andar++) {
+                        printf("Elevador subindo\n");
                         printf("%i\n", andar);
                         sleep(1);
+                        printf("\033[H\033[J");
                     }
                 }
                 andar = andarDesejado;
                 printf("Voce chegou ao andar %i\n", andarDesejado);
-                
+
             } else printf("Andar inválido\n");
         } else printf("Voce ja se encontra no andar certo\n");
     }
