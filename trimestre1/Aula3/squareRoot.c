c8ec104f8f9d60d9f975a13e31b222c802f431a0
vicenzoStopa <vicenzoStopa>
2026-02-02 20:26:41 -0300

    fixes

--- trimestre1/Aula3/squareRoot.c
+++ trimestre1/Aula3/squareRoot.c
@@ -6,7 +6,7 @@ int main() {
     float valor;
     float respostaRaizQuadrada;
 
-    printf("Informe o valor que se deseja calcular >> ");
+    printf("Informe o valor que deseja calcular >> ");
     scanf("%f", &valor);
     respostaRaizQuadrada = sqrt(valor);
     printf("O resultado da raiz de %.1f: %.2f", valor, respostaRaizQuadrada);
